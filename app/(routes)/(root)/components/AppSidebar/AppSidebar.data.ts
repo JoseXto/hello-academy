9d0ae312086dc1841f48bbf66bcc409cc15518a1
JoseXto <jose16xto@gmail.com>
2025-11-29 23:57:13 -0600

    sidebar

---
+++ app/(routes)/(root)/components/AppSidebar/AppSidebar.data.ts
@@ -0,0 +1,24 @@
+import { BookOpen, House, Settings2, SquareTerminal } from "lucide-react";
+
+export const routes = [
+    {
+        title: "Home",
+        url: "/",
+        icon: House
+    },
+    {
+        title: "Cursos",
+        url: "/courses",
+        icon: SquareTerminal
+    },
+    {
+        title: "Mis cursos",
+        url: "/my-courses",
+        icon: BookOpen
+    },
+    {
+        title: "Ajustes",
+        url: "/settings",
+        icon: Settings2
+    },
+]
